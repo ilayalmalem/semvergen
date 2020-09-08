a8b820e111ddda355ecf5c5be2262e87bbbd42d6
Ilay Almalem <ilayalmalem@gmail.com>
2020-09-08 23:38:55 +0300

    1.0.19
    [object Object]

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 #! /usr/bin/env node
-const config = require('./package.json');
+const config = require("./package.json");
 const { exec } = require("child_process");
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 String.prototype.replaceAt = function (index, replacement) {
-    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+    return (this.substr(0, index) +
+        replacement +
+        this.substr(index + replacement.length));
 };
 var semver;
 (function (semver) {
     semver.versions = [];
     function seedVersions() {
         for (let index = 0; index <= 100; index++) {
-            var base = '1.0.0';
-            var buildNumber = base.split('.');
+            var base = "1.0.0";
+            var buildNumber = base.split(".");
             var build = {
                 MAJOR: buildNumber[0],
                 MID: buildNumber[1],
                 MINOR: buildNumber[2],
             };
-            let v = base.lastIndexOf('0');
+            let v = base.lastIndexOf("0");
             base = base.replaceAt(v, `${index}`);
             semver.versions.push(`${base}`);
         }
@@ -26,34 +28,47 @@ var semver;
     semver.seedVersions = seedVersions;
     function getNextVersion(version, method) {
         var v = semver.versions.indexOf(version);
-        if (method === 'MINOR_PATCH')
+        if (method === "MINOR_PATCH")
             return semver.versions[++v];
     }
     semver.getNextVersion = getNextVersion;
 })(semver || (semver = {}));
 const [, , ...args] = process.argv;
-if (args.includes('build') && args.includes('current')) {
+if (args.includes("build") && args.includes("current")) {
     semver.seedVersions();
-    var newVer = semver.getNextVersion(config.version, 'MINOR_PATCH');
-    console.log('Commiting your work to github.');
-    exec('git add .', (error, stdout, stderr) => {
+    var newVer = semver.getNextVersion(config.version, "MINOR_PATCH");
+    console.log("Commiting your work to github.");
+    exec("git add .", (error, stdout, stderr) => {
         if (error) {
             return;
         }
         if (stderr) {
             return;
         }
-    });
-    inquirer.prompt([{ type: 'input', name: 'message', message: 'Type commit message', },])
-        .then(message => {
-        exec(`git commit -m "${newVer}" -m "${message}"`, (error, stdout, stderr) => {
-            if (error) {
-                return;
-            }
-            if (stderr) {
-                return;
-            }
-            exec('git push origin master', (error, stdout, stderr) => {
+        inquirer
+            .prompt([
+            { type: "input", name: "message", message: "Type commit message" },
+        ])
+            .then((message) => {
+            exec(`git commit -m "${newVer}" -m "${message}"`, (error, stdout, stderr) => {
+                if (error) {
+                    return;
+                }
+                if (stderr) {
+                    return;
+                }
+                exec("git push origin master", (error, stdout, stderr) => {
+                    if (error) {
+                        return;
+                    }
+                    if (stderr) {
+                        return;
+                    }
+                });
+            });
+        })
+            .then(() => {
+            exec(`npm version ${newVer}`, (error, stdout, stderr) => {
                 if (error) {
                     return;
                 }
@@ -61,24 +76,14 @@ if (args.includes('build') && args.includes('current')) {
                     return;
                 }
             });
-        });
-    })
-        .then(() => {
-        exec(`npm version ${newVer}`, (error, stdout, stderr) => {
-            if (error) {
-                return;
-            }
-            if (stderr) {
-                return;
-            }
-        });
-        exec(`npm publish`, (error, stdout, stderr) => {
-            if (error) {
-                return;
-            }
-            if (stderr) {
-                return;
-            }
+            exec(`npm publish`, (error, stdout, stderr) => {
+                if (error) {
+                    return;
+                }
+                if (stderr) {
+                    return;
+                }
+            });
         });
     });
 }
--- index.ts
+++ index.ts
@@ -1,94 +1,99 @@
 #! /usr/bin/env node
-const config = require('./package.json')
+const config = require("./package.json");
 const { exec } = require("child_process");
-const inquirer = require('inquirer')
-
+const inquirer = require("inquirer");
 
 declare interface String {
-    replaceAt(index, replacement) : string;
+  replaceAt(index, replacement): string;
 }
 
-String.prototype.replaceAt = function (index, replacement) {
-    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+String.prototype.replaceAt = function(index, replacement) {
+  return (
+    this.substr(0, index) +
+    replacement +
+    this.substr(index + replacement.length)
+  );
 };
 
-
 namespace semver {
-    export const versions = []
-    export function seedVersions() {
-        for (let index = 0; index <= 100; index++) {
-            var base: String = '1.0.0'
-            var buildNumber = base.split('.')
-            var build = {
-                MAJOR:buildNumber[0],
-                MID:buildNumber[1],
-                MINOR:buildNumber[2],
-            }
-            let v: number = base.lastIndexOf('0');
-            base = base.replaceAt(v, `${index}`)
-            versions.push(`${base}`)
-        }
-        return versions
+  export const versions = [];
+  export function seedVersions() {
+    for (let index = 0; index <= 100; index++) {
+      var base: String = "1.0.0";
+      var buildNumber = base.split(".");
+      var build = {
+        MAJOR: buildNumber[0],
+        MID: buildNumber[1],
+        MINOR: buildNumber[2],
+      };
+      let v: number = base.lastIndexOf("0");
+      base = base.replaceAt(v, `${index}`);
+      versions.push(`${base}`);
     }
+    return versions;
+  }
 
-    export function getNextVersion(version, method) {
-        var v = versions.indexOf(version);
-        if(method === 'MINOR_PATCH') return versions[++v]
-    }
-
+  export function getNextVersion(version, method) {
+    var v = versions.indexOf(version);
+    if (method === "MINOR_PATCH") return versions[++v];
+  }
 }
 
-const [,, ...args] = process.argv
+const [, , ...args] = process.argv;
 
-if(args.includes('build') && args.includes('current')) {
-    semver.seedVersions()
-    var newVer = semver.getNextVersion(config.version,'MINOR_PATCH');
-    console.log('Commiting your work to github.')
-    exec('git add .', (error, stdout, stderr) => {
-        if (error) {
-            return;
-        }
-        if (stderr) {
-            return;
-        }
-    })
-
-    inquirer.prompt([{type: 'input',name: 'message',message: 'Type commit message',},])
-    .then(message => {
-        exec(`git commit -m "${newVer}" -m "${message}"`, (error, stdout, stderr) => {
+if (args.includes("build") && args.includes("current")) {
+  semver.seedVersions();
+  var newVer = semver.getNextVersion(config.version, "MINOR_PATCH");
+  console.log("Commiting your work to github.");
+  exec("git add .", (error, stdout, stderr) => {
+    if (error) {
+      return;
+    }
+    if (stderr) {
+      return;
+    }
+    inquirer
+      .prompt([
+        { type: "input", name: "message", message: "Type commit message" },
+      ])
+      .then((message) => {
+        exec(
+          `git commit -m "${newVer}" -m "${message}"`,
+          (error, stdout, stderr) => {
             if (error) {
-                return;
+              return;
             }
             if (stderr) {
-                return;
+              return;
             }
-            exec('git push origin master', (error, stdout, stderr) => {
-                if (error) {
-                    return;
-                }
-                if (stderr) {
-                    return;
-                }
-            })
-        })
-    })
-    .then(() => {
-    
+            exec("git push origin master", (error, stdout, stderr) => {
+              if (error) {
+                return;
+              }
+              if (stderr) {
+                return;
+              }
+            });
+          }
+        );
+      })
+      .then(() => {
         exec(`npm version ${newVer}`, (error, stdout, stderr) => {
-            if (error) {
-                return;
-            }
-            if (stderr) {
-                return;
-            }
-        })
+          if (error) {
+            return;
+          }
+          if (stderr) {
+            return;
+          }
+        });
         exec(`npm publish`, (error, stdout, stderr) => {
-            if (error) {
-                return;
-            }
-            if (stderr) {
-                return;
-            }
-        })
-    })
+          if (error) {
+            return;
+          }
+          if (stderr) {
+            return;
+          }
+        });
+      });
+  });
 }
